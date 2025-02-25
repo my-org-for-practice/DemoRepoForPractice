069baf8d16994dd77cff31aaa1ed07cca9a03aff
Jayaprakash Golla <jayatwo.7501@gmail.com>
2025-02-25 12:03:18 +0530

    Update Tester.java
    Added code for finding a num is even or odd

--- Tester.java
+++ Tester.java
@@ -1,5 +1,11 @@
 public class Tester{
   public static void main(String [] args){
-    
+    //
+    int n=89;
+    if(n%2==0){
+      System.out.println("even");
+    }
+    else
+      System.out.println("Odd");
   }
 }
